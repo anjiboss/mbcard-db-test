e785dcdc4de609e0792ab171c0c1ae5097c89579
Anjiboss <nhu.nhu.face@gmail.com>
2021-06-06 01:26:38 +0900

    inprogress

--- back-end/utils/registerUser.js
+++ back-end/utils/registerUser.js
@@ -2,25 +2,38 @@ const User = require("../models/User");
 
 const registerUser = async (userInfo, res) => {
   const { username, email, password } = userInfo;
-
+  console.log(username);
   // Check If User Is Existed
   try {
-    User.findOne({ username: username }, async (e, user) => {
-      if (user) {
-        res.send("User Existed");
-      } else {
-        try {
+    User.find(
+      { $or: [{ username: username }, { email: email }] },
+      async (e, user) => {
+        console.log(user);
+        if (user.toString() !== "") {
+          res.send("User Existed");
+        } else {
           const user = new User({
             username: username,
             email: email,
             password: password,
           });
-          await user.save(() => res.send("save successfully"));
-        } catch (error) {
-          console.log(error);
+          user.save((e) => {
+            if (e) {
+              res.json({
+                success: false,
+                error: e.message,
+              });
+              console.log(e.message);
+            } else {
+              res.json({
+                success: true,
+                user,
+              });
+            }
+          });
         }
       }
-    });
+    );
   } catch (error) {
     console.log(error);
   }
