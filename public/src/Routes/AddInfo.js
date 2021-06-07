711ea7f055e349fe3c108a62238c2603a6f8dd2c
Anjiboss <nhu.nhu.face@gmail.com>
2021-06-08 03:52:33 +0900

    add info css

--- public/src/Routes/AddInfo.js
+++ public/src/Routes/AddInfo.js
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import "../css/form.css";
+import "../css/addInfo.css";
 
 const AddInfo = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [altPhone, setAltPhone] = useState("");
+  const [facebook, setFacebook] = useState("");
+  const [instagram, setInstagram] = useState("");
 
   const addInfoHandler = (e) => {
     e.preventDefault();
@@ -13,7 +18,7 @@ const AddInfo = () => {
   return (
     <>
       <h1>Add Info</h1>
-      <div className="form">
+      <div className="form" id="add-info-form">
         <form autoComplete="off" onSubmit={addInfoHandler}>
           <div className="form-input">
             <h4>Username</h4>
@@ -63,6 +68,55 @@ const AddInfo = () => {
             />
           </div>
 
+          <div className="form-input">
+            <h4>Phone Number</h4>
+            <input
+              type="text"
+              name="phone"
+              value={phone}
+              onChange={(e) => {
+                setPhone(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="form-input">
+            <h4>Alternative Phone Number</h4>
+            <input
+              type="text"
+              name="alt-phone"
+              value={altPhone}
+              onChange={(e) => {
+                setAltPhone(e.target.value);
+              }}
+            />
+          </div>
+          <div className="sns">
+            <h4>SNS</h4>
+            <div className="form-input">
+              <h5>Facebook</h5>
+              <input
+                type="text"
+                name="facebook"
+                value={facebook}
+                onChange={(e) => {
+                  setFacebook(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="form-input">
+              <h5>Instagram</h5>
+              <input
+                type="text"
+                name="instagram"
+                value={instagram}
+                onChange={(e) => {
+                  setInstagram(e.target.value);
+                }}
+              />
+            </div>
+          </div>
           <button className="btn" type="submit">
             Submit
           </button>
